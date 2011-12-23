5857a24ef0eaa0835f9e9c5a5f80ce681a1e801e
Franklin S Cooper Jr <fcooper@ti.com>
2011-12-22 16:29:40 -0800

    matrix_browser: Accept commandline argument
    *Use the first parameter as the url to view instead of the hardcoded value
    
    Signed-off-by: Franklin S Cooper Jr <fcooper@ti.com>

--- main.cpp
+++ main.cpp
@@ -1,17 +1,23 @@
 #include <QtGui>
 #include <QWebView>
 #include <QGraphicsWebView>
+#include <iostream> 
+
 int main(int argc, char **argv)
 {
   QApplication app(argc, argv);
+  QStringList args = app.arguments();
+  if (args.count() != 2)
+  {
+    std::cerr << "You need to pass the URL you want to view\n" << endl;
+    return 1;
+  }
+
   QWebView view;
-  view.load(QUrl("http://localhost:8080"));
+  view.load(QUrl(args[1]));
   view.setContextMenuPolicy(Qt::NoContextMenu);
   view.setWindowFlags(Qt::FramelessWindowHint);
   view.showMaximized();
   view.show();
   return app.exec();
 }
-
-
-
